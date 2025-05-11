065e0cf151b9d30c94c8ee3d1609a94a8f6e64c3
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-11 19:40:32 +0530

    Added purge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*"
+  ],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
