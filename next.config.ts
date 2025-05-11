b0e62159f860a9b29ab338d84934284298a0010d
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-11 13:28:58 +0530

    Added output export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
  
 const nextConfig: NextConfig = {
-// output: 'export',
+output: 'export',
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
    images: { unoptimized: true },
