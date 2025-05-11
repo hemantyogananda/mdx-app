6f349f7caadfb90506acb8907057c2ae8685e7c3
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-11 13:12:16 +0530

    Testing trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ output: 'export',
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
    images: { unoptimized: true },
+   trailingSlash: false,
   // Optionally, add any other Next.js config below
 }
 
