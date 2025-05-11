d1e5d74bd787b38b99e95457868c7c20c7690d84
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-11 11:58:19 +0530

    Mobx Blog

--- app/mdx-page/page.tsx
+++ app/mdx-page/page.tsx
@@ -1,7 +1,7 @@
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { posts } from "@/posts";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
@@ -12,7 +12,6 @@ export default function Home() {
       {/* hero */}
       <div className="mb-20 px-5 md:px-0">
         <div className="h-[250px] md:h-[600px] rounded-md relative">
-          <Image src={"/images/hero.png"} alt="hero image" sizes="100vh" fill />
           <div className="absolute -bottom-8 bg-white dark:bg-[#242535] p-6 ml-10 rounded-lg shadow-lg max-w-[80%] md:max-w-[40%]">
             <p className="text-xs bg-blue-700 w-fit py-1 px-2 text-white rounded-md mb-1">
               Technology
@@ -34,16 +33,6 @@ export default function Home() {
             href={`/blog/${p.slug}`}
             className="p-4 group rounded-lg border w-[392px] border-gray-200 dark:border-gray-700"
           >
-            {/* image */}
-            <div className="h-60 w-full relative overflow-hidden rounded-md object-cover group-hover:scale-105 duration-300 transition-all">
-              <Image
-                src={p.thumbnail}
-                alt={`${p.title} - thumbnail`}
-                sizes="100vh"
-                fill
-              />
-            </div>
-
             {/* category */}
             <p className="text-sm bg-gray-100 dark:bg-gray-700/95 text-blue-700 dark:text-blue-500 font-semibold my-4 w-fit px-2 py-1 rounded-sm">
               {p.category}
