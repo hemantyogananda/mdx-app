6627fbd4b167b536623e3743116119695320621f
team ambatturvasi <teamambatturvasi@gmail.com>
2025-05-13 21:47:14 +0530

    Testing tailwind

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"dpqC+3xrkUOWT6XD7yHcIkzd866GhO9d3aTos+lp4Wg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
